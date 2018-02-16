1b179937326bcee53dd25eb4b8e6a391233eb751
hut <hut@hut.pm>
2018-02-16 19:47:48 +0100

    Remove ellipsis from foldtext

--- ftplugin/orgmode/_vim.py
+++ ftplugin/orgmode/_vim.py
@@ -220,7 +220,7 @@ def fold_text(allow_dirty=False):
 		str_heading = re.sub(r'\*(?=\*)', ' ', str_heading)  # Make leading n-1 asterisks invisible
 
 		# Workaround for vim.command seems to break the completion menu
-		vim.eval(u_encode(u'SetOrgFoldtext("%s...")' % (re.sub(r'\[\[([^[\]]*\]\[)?([^[\]]+)\]\]', r'\2',
+		vim.eval(u_encode(u'SetOrgFoldtext("%s")' % (re.sub(r'\[\[([^[\]]*\]\[)?([^[\]]+)\]\]', r'\2',
 				str_heading).replace( u'\\', u'\\\\').replace(u'"', u'\\"'), )))
 
 
